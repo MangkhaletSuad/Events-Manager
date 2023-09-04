01893a0115412d3735308ff04771f782899a1fe7
MangkhaletSuad <mangkhalet.suad@gmail.com>
2023-09-04 02:48:07 +0000

    Added First

---
+++ src/rvcard.js
@@ -0,0 +1,33 @@
+class RVcard extends HTMLElement {
+
+    config;
+    content;
+
+    // required
+    setConfig(config) {
+        this.config = config;
+    }
+
+    set hass(hass) {
+        const entityId = this.config.entity;
+        const state = hass.states[entityId];
+        const stateStr = state ? state.state : 'unavailable';
+
+        // done once
+        if (!this.content) {
+            // user makes sense here as every login gets it's own instance
+            this.innerHTML = `
+                <ha-card header="Hello ${hass.user.name}!">
+                    <div class="card-content" ${hass.entity.camera}></div>
+                </ha-card>
+            `;
+            this.content = this.querySelector('div');
+        }
+        // done repeatedly
+        this.content.innerHTML = `
+            <p>The ${entityId} is ${stateStr}.</p>
+        `;
+    }
+}
+
+customElements.define('rv-card', RVcard);
