90548b1f8834e69e8af56456c4d9b36d335ef047
MangkhaletSuad <mangkhalet.suad@gmail.com>
2023-09-04 09:37:30 +0000

    try

--- rvcard.js
+++ rvcard.js
@@ -1,51 +1,39 @@
-// import { LitElement, html, css } from 'lit-element';
-{/* <script type="module" src="https://cdn.jsdelivr.net/npm/lit-element@2.5.0/lit-element.js"></script> */}
-
 class RVcard extends HTMLElement {
-    static styles = css`
-    .content {
-        overflow: hidden;
+  _hass
+  config;
+  content;
+
+
+  setConfig(config) {
+      if (!config.entity) {
+          throw new Error('Please define an entity!');
       }
-      .content hui-card-preview {
-        max-width: 100%;
-      }
-      ha-card {
-        height: 100%;
-        overflow: hidden;
-      }
-  `;
-    render() {
-        return html`
+      this.config = config;
+  }
+
+  set hass(hass) {
+      this._hass = hass;
+      const entityId = this.config.entity;
+      const state = hass.states[entityId];
+      const stateStr = state ? state.state : 'unavailable';
+
+      // done once
+      if (!this.content) {
+          // user makes sense here as every login gets it's own instance
+          this.innerHTML = `
           <ha-card header="Hello user ${hass.user.name}">
-            <img src="[[cameraEntityImageUrl]]" />
+              <div class="card-content"></div>
+              <hui-image img= "/workspaces/core/config/www/Events-Manager/rvc.jpeg">
+              </hui-image>
           </ha-card>
-        `;
+          `;
+          this.content = this.querySelector('div');
       }
-
-      static get properties() {
-        return {
-          hass: { type: Object },
-          config: { type: Object },
-          cameraEntityImageUrl: { type: String },
-        };
-      }
-
-    static getStubConfig() {
-        return { entity: "sun.sun" }
-    }
-
-    setConfig(config) {
-        if (!config.camera_entity) {
-          throw new Error('Missing camera_entity in card configuration');
-        }
-        this.config = config;
-      }
-
-      set hass(hass) {
-        this.cameraEntityImageUrl = hass.states[this.config.camera_entity].attributes.entity_picture;
-        this._hass = hass;
-      }
-    }
+  }
+  static getStubConfig() {
+      return { entity: "sun.sun" }
+  }
+}
 
 
 
@@ -53,9 +41,8 @@ customElements.define("rv-card", RVcard);
 
 window.customCards = window.customCards || [];
 window.customCards.push({
-  type: "rv-card",
-  name: "RVcard",
-  preview: false, // Optional - defaults to false
+type: "rv-card",
+name: "RVcard",
+preview: false, // Optional - defaults to false
 
 });
-
