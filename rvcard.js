d0d252318ea604e7778a09694c581973febb66e6
MangkhaletSuad <mangkhalet.suad@gmail.com>
2023-09-05 16:55:56 +0700

    lit-v-3

--- rvcard.js
+++ rvcard.js
@@ -328,6 +328,33 @@ export class RVcard extends LitElement{
       `;
     }
 
+    throttle(callback, delay) {
+      let isThrottled = false,
+        args,
+        context;
+  
+      function wrapper() {
+        if (isThrottled) {
+          args = arguments;
+          context = this;
+          return;
+        }
+  
+        isThrottled = true;
+        callback.apply(this, arguments);
+  
+        setTimeout(() => {
+          isThrottled = false;
+          if (args) {
+            wrapper.apply(context, args);
+            args = context = null;
+          }
+        }, delay);
+      }
+  
+      return wrapper;
+    }
+
   }
 
 window.customCards = window.customCards || [];
