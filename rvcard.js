46094376ec99f73cf3a8a5310643f40b47ee023d
MangkhaletSuad <mangkhalet.suad@gmail.com>
2023-09-05 03:43:18 +0000

    Test-2

--- rvcard.js
+++ rvcard.js
@@ -21,27 +21,15 @@ class RVcard extends HTMLElement{
       <img src="http://via.placeholder.com/350x150">
     `;
   }
-
-      // static get properties() {
-      //   return {
-      //     config: { type: Object },
-      //     cameraEntityImageUrl: { type: String },
-      //   };
-      // }
   setConfig(config) {
       if (!config.camera_entity) {
         throw new Error('Missing camera_entity in card configuration');
       }
         this.config = config;
-      }
-
-  // set hass(value) {
-  //   const entityId = this.config.entity;
-  //   const state = hass.states[entityId];
-  //   const stateStr = state ? state.state : "unavailable";
-  //   this.cameraEntityImageUrl = value.states[this.config.camera_entity].attributes.entity_picture;
-  //   this._hass = value;
-  // }
+  }
+  getCardSize() {
+    return 3;
+  }
 }
 
 
@@ -52,7 +40,5 @@ window.customCards = window.customCards || [];
 window.customCards.push({
   type: "rv-card",
   name: "RVcard",
-  preview: false, // Optional - defaults to false
-
 });
 
