61353a559afbef3bbc64c85a41c362d48e57174e
MangkhaletSuad <mangkhalet.suad@gmail.com>
2023-09-07 10:00:11 +0700

    Add-paho

--- rvcard.js
+++ rvcard.js
@@ -18,87 +18,6 @@ class RVcard extends HTMLElement{
       `;
       this.content = this.querySelector("div");
     }
-      const options = {
-        host: '192.168.51.101',
-        port: 8000,
-        username: 'rvc',
-        password: 'P@ssw0rd'
-      };
-
-      const client = new Paho.MQTT.Client(options.host,options.port,this.clientID);
-
-      client.onConnectionLost = onConnectionLost;
-      client.onMessageArrived = onMessageArrived;
-
-      client.connect({
-        onSuccess: function () {
-          onConnect();
-          onConnectTime();
-          onConnectPosition();
-        },
-        userName: "rvc",
-        password: "P@ssw0rd"
-
-      });
-
-      function onConnect(){
-        console.log("onConnect");
-        topic =  "Name";
-        // document.getElementById("messages").innerHTML += "<span> Subscribing to topic "+topic + "</span><br>";
-        client.subscribe(topic);
-      }
-
-      function onMessageArrived(message){
-          console.log("onMessageArrived");
-          console.log("OnMessageArrived: "+message.payloadString);
-          // document.getElementById("messages").innerHTML += "<span> Topic:"+message.destinationName+"| Message : "+message.payloadString + "</span><br>";
-          const messagesDiv = document.getElementById("messages");
-          messagesDiv.innerHTML += "<span> Topic:" + message.destinationName + "| Message : " + message.payloadString + "</span><br>";
-      }
-
-      function onConnectTime() {
-          console.log("onConnectTime");
-          topicTime =  "Time";
-          // document.getElementById("messages").innerHTML += "<span> Subscribing to topic "+topicTime + "</span><br>";
-          client.subscribe(topicTime);
-      }
-
-      function onMessageArrivedTime(message){
-          console.log("onMessageArrivedTmie");
-          console.log("OnMessageArrived: "+message.payloadString);
-          // document.getElementById("messages").innerHTML += "<span> Topic:"+message.destinationName+"| Message : "+message.payloadString + "</span><br>";
-          const messagesDiv = document.getElementById("messages");
-          messagesDiv.innerHTML += "<span>" + message.destinationName + ": " + message.payloadString + "</span><br>";
-      }
-
-      function onConnectPosition() {
-          console.log("onConnectPosition");
-          topicPosition =  "Position";
-          // document.getElementById("messages").innerHTML += "<span> Subscribing to topic "+topicTime + "</span><br>";
-          client.subscribe(topicPosition);
-      }
-
-      function onMessageArrivedPosition(message){
-          console.log("onMessageArrivedPosition");
-          console.log("OnMessageArrived: "+message.payloadString);
-          // document.getElementById("messages").innerHTML += "<span> Topic:"+message.destinationName+"| Message : "+message.payloadString + "</span><br>";
-          const messagesDiv = document.getElementById("messages");
-          messagesDiv.innerHTML += "<span>" + message.destinationName + ": " + message.payloadString + "</span><br>";
-      }
-
-      function onConnectionLost(responseObject){
-          console.log("onConnectionLost");
-          document.getElementById("messages").innerHTML += "<span> ERROR: Connection is lost.</span><br>";
-          if(responseObject !=0){
-              document.getElementById("messages").innerHTML += "<span> ERROR:"+ responseObject.errorMessage +"</span><br>";
-          }
-      }
-
-      function startDisconnect(){
-          console.log("startDisconnect");
-          client.disconnect();
-          document.getElementById("messages").innerHTML += "<span> Disconnected. </span><br>";
-      }
 
     const entityId = this.config.entity;
     const state = hass.states[entityId];
@@ -106,7 +25,9 @@ class RVcard extends HTMLElement{
 
     this.content.innerHTML = `
     <head>
+    <script src="./paho.javascript-1.0.3/paho-mqtt-min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.2/mqttws31.min.js" type="text/javascript"></script>
+    <script src="mqtt.js"></script>
     <link rel="stylesheet" href="test.css">
     </head>
     <body>
