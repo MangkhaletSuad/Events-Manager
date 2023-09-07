80554a67d5ecb45317dc014fcd551d8c36c6f603
MangkhaletSuad <mangkhalet.suad@gmail.com>
2023-09-07 10:00:05 +0700

    Add-paho

--- mqtt.js
+++ mqtt.js
@@ -1,40 +1,82 @@
-function submessage () {
-  const mqtt = require('mqtt');
-  const options = {
-      host: '192.168.51.101',
-      port: 1883,
-      username: 'rvc',
-      password: 'P@ssw0rd'
-    };
+clientID = "clientID - "+parseInt(Math.random() * 100);
+        const options = {
+        host: '192.168.51.101',
+        port: 8000,
+        username: 'rvc',
+        password: 'P@ssw0rd'
+        };
 
-    // mqtt.readFile('/test.html', function(error, html) {
-    //   if (error) throw error;
-    //   HTMLOutputElement.createServer(function(request, response){
-    //     response.writeHeader(200, {"Content-Type": "text/html"});
-    //     response.write(html);
-    //     response.end();
-    //   }).listen(PORT)
-    // });
+        const client = new Paho.MQTT.Client(options.host,options.port,clientID);
 
-    const client = mqtt.connect(options);
+        client.onConnectionLost = onConnectionLost;
+        client.onMessageArrived = onMessageArrived;
 
-    client.on('connect', function () {
-      console.log('Connected')
-      client.subscribe('Test', function (err) {
-        if (!err) {
-          client.publish('Test', 'Hello mqtt')
+        client.connect({
+            onSuccess: function () {
+                onConnect(); 
+                onConnectTime();
+                onConnectPosition();
+            },
+            userName: "rvc",
+            password: "P@ssw0rd"
+
+        });
+
+        function onConnect(){
+            console.log("onConnect");
+            topic =  "Name";
+            // document.getElementById("messages").innerHTML += "<span> Subscribing to topic "+topic + "</span><br>";
+            client.subscribe(topic);
         }
-      });
-    });
 
-    client.on('message', function (topic, message) {
-      console.log(`Received message on topic ${topic}: ${message}`);
-      updateHTML(message.toString());
-    });
+        function onMessageArrived(message){
+            console.log("onMessageArrived");
+            console.log("OnMessageArrived: "+message.payloadString);
+            // document.getElementById("messages").innerHTML += "<span> Topic:"+message.destinationName+"| Message : "+message.payloadString + "</span><br>";
+            const messagesDiv = document.getElementById("messages");
+            messagesDiv.innerHTML += "<span> Topic:" + message.destinationName + "| Message : " + message.payloadString + "</span><br>";
+        }
 
-    function updateHTML(message) {
-      const messageElement = document.getElementById('mqtt-message');
-      messageElement.innerHTML = message;
-    }
+        function onConnectTime() {
+            console.log("onConnectTime");
+            topicTime =  "Time";
+            // document.getElementById("messages").innerHTML += "<span> Subscribing to topic "+topicTime + "</span><br>";
+            client.subscribe(topicTime);
+        }
 
-}
+        function onMessageArrivedTime(message){
+            console.log("onMessageArrivedTmie");
+            console.log("OnMessageArrived: "+message.payloadString);
+            // document.getElementById("messages").innerHTML += "<span> Topic:"+message.destinationName+"| Message : "+message.payloadString + "</span><br>";
+            const messagesDiv = document.getElementById("messages");
+            messagesDiv.innerHTML += "<span>" + message.destinationName + ": " + message.payloadString + "</span><br>";
+        }
+
+        function onConnectPosition() {
+            console.log("onConnectPosition");
+            topicPosition =  "Position";
+            // document.getElementById("messages").innerHTML += "<span> Subscribing to topic "+topicTime + "</span><br>";
+            client.subscribe(topicPosition);
+        }
+
+        function onMessageArrivedPosition(message){
+            console.log("onMessageArrivedPosition");
+            console.log("OnMessageArrived: "+message.payloadString);
+            // document.getElementById("messages").innerHTML += "<span> Topic:"+message.destinationName+"| Message : "+message.payloadString + "</span><br>";
+            const messagesDiv = document.getElementById("messages");
+            messagesDiv.innerHTML += "<span>" + message.destinationName + ": " + message.payloadString + "</span><br>";
+        }
+
+        function onConnectionLost(responseObject){
+            console.log("onConnectionLost");
+            document.getElementById("messages").innerHTML += "<span> ERROR: Connection is lost.</span><br>";
+            if(responseObject !=0){
+                document.getElementById("messages").innerHTML += "<span> ERROR:"+ responseObject.errorMessage +"</span><br>";
+            }
+        }
+
+        function startDisconnect(){
+            console.log("startDisconnect");
+            client.disconnect();
+            document.getElementById("messages").innerHTML += "<span> Disconnected. </span><br>";
+        }
